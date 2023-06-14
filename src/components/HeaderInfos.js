dad19def57c47f1bbf6311cfb3da6540819c8983
Maxime Monnier <maximemonnier39@gmail.com>
2023-06-14 15:08:55 +0400

    build treemap

--- src/components/HeaderInfos.js
+++ src/components/HeaderInfos.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import PercentChange from "./PercentChange";
+import TablesFilters from "./TablesFilters";
 
 const HeaderInfos = () => {
   const [HeaderData, setHeaderData] = useState([]);
@@ -13,41 +14,40 @@ const HeaderInfos = () => {
   }, []);
 
   return (
-    <header>
-      <div className="header-container">
-        <ul className="title">
-          <li>
-            <h1>
-              <img id="toto" src="./assets/logo.png" alt="logo" /> Watch tower
-            </h1>
-          </li>
-          <li>
-            crypto-monnaies :{" "}
-            {HeaderData.active_cryptocurrencies &&
-              HeaderData.active_cryptocurrencies.toLocaleString()}
-          </li>
-          <li>Marchés : {HeaderData.markets && HeaderData.markets}</li>
-        </ul>
-        <ul className="infos-mkt">
-          <li className="global-mkt">
-            Global Market Cap :
-            <PercentChange
-              percent={HeaderData.market_cap_change_percentage_24h_usd}
-            />
-          </li>
-          <li>
-            BTC dominance :{" "}
-            {HeaderData.market_cap_percentage &&
-              HeaderData.market_cap_percentage.btc.toFixed(2) + "%"}
-          </li>
-          <li>
-            ETH dominance :{" "}
-            {HeaderData.market_cap_percentage &&
-              HeaderData.market_cap_percentage.eth.toFixed(2) + "%"}
-          </li>
-        </ul>
-      </div>
-    </header>
+    <div className="header-container">
+      <ul className="title">
+        <li>
+          <h1>
+            <img id="toto" src="./assets/logo.png" alt="logo" /> Watch tower
+          </h1>
+        </li>
+        <li>
+          crypto-monnaies :{" "}
+          {HeaderData.active_cryptocurrencies &&
+            HeaderData.active_cryptocurrencies.toLocaleString()}
+        </li>
+        <li>Marchés : {HeaderData.markets && HeaderData.markets}</li>
+      </ul>
+      <ul className="infos-mkt">
+        <li className="global-mkt">
+          Global Market Cap :
+          <PercentChange
+            percent={HeaderData.market_cap_change_percentage_24h_usd}
+          />
+        </li>
+        <li>
+          BTC dominance :{" "}
+          {HeaderData.market_cap_percentage &&
+            HeaderData.market_cap_percentage.btc.toFixed(2) + "%"}
+        </li>
+        <li>
+          ETH dominance :{" "}
+          {HeaderData.market_cap_percentage &&
+            HeaderData.market_cap_percentage.eth.toFixed(2) + "%"}
+        </li>
+      </ul>
+      <TablesFilters />
+    </div>
   );
 };
 
--- src/components/TablesFilters.js
+++ src/components/TablesFilters.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const TablesFilters = () => {
-  return <div></div>;
+  return (
+    <div className="table-filters">
+      <div className="table-filters-container">
+        <div className="stable-checkbox-container">
+          <input type="checkbox" id="stableCoin" defaultChecked={true} />
+          <label htmlFor="stableCoin">Avec stable coin</label>
+        </div>
+        <div className="no-list-btn">
+          <p>Aucun liste</p>
+        </div>
+        <div className="fav-list">
+          <p>liste des favoris</p>
+          <img src="./assets/star-full.svg" alt="icon star" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TablesFilters;
